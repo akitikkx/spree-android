bf4626a551fd82af3180e8a99c55ef6a44771ef8
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2017-11-12 20:25:55 +0200

    [changes] updated firebase crash

--- app/src/androidTest/java/za/co/ahmedtikiwa/apps/spree/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/za/co/ahmedtikiwa/apps/spree/ExampleInstrumentedTest.kt
@@ -2,11 +2,9 @@ package za.co.ahmedtikiwa.apps.spree
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("za.co.ahmedtikiwa.apps.sacp", appContext.packageName)
+        assertEquals("za.co.ahmedtikiwa.apps.spree", appContext.packageName)
     }
 }
